50761fc867c585bea61ba466c2f3cc991945d0bb
Josue MTL <116099684+JosueMTL@users.noreply.github.com>
2023-11-17 10:46:21 -0500

    true

---
+++ src/main/kotlin/com/FullStacks/Assitant/AssitantApplication.kt
@@ -0,0 +1,11 @@
+package com.FullStacks.Assitant
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssitantApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssitantApplication>(*args)
+}
---
+++ src/test/kotlin/com/FullStacks/Assitant/AssitantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.FullStacks.Assitant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssitantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
