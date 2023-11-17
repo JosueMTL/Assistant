882d5d66503415b2a8c17f4a8c9cc5857871ea9a
Josue MTL <116099684+JosueMTL@users.noreply.github.com>
2023-11-17 15:36:14 -0500

    commit_2

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS Users(
+    id SERIAL,
+    namee VARCHAR (20) NOT NULL,
+    last_name VARCHAR (20) NOT NULL,
+    Contact_info VARCHAR (20) NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Reminders(
+
+)
